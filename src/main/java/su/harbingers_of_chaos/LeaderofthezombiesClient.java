cc70e0f42358e4944682b868b770b579e98bd128
NoDarckName <ssskkssllaavvaa@gmail.com>
2024-09-01 19:34:29 +0300

    create

---
+++ src/main/java/su/harbingers_of_chaos/LeaderofthezombiesClient.java
@@ -0,0 +1,10 @@
+package su.harbingers_of_chaos;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LeaderofthezombiesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
